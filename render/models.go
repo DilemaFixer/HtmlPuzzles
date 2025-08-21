54f7e14b0fb740ecb2f735c88485b41762f6a852
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-08-21 17:40:33 +0200

    Add base models

---
+++ render/models.go
@@ -0,0 +1,11 @@
+package render
+
+import (
+	htmlparser "github.com/DilemaFixer/HtmlPuzzles/html"
+	"github.com/DilemaFixer/HtmlPuzzles/tools"
+)
+
+type HtmlRenderer interface {
+	AddChildren(HtmlRenderer)
+	Render(tools.Context) *htmlparser.HtmlTag
+}
